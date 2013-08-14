96e0ecaa3d365e6e42579a60ed9556c5efdc7d35
Jouko Johansson <joukojo@gmail.com>
2013-08-15 00:25:53 +0300

    added eception handling, fixed url

--- src/main/java/org/yogocodes/bikewars/web/personal/UserInfoController.java
+++ src/main/java/org/yogocodes/bikewars/web/personal/UserInfoController.java
@@ -1,5 +1,7 @@
 package org.yogocodes.bikewars.web.personal;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.yogocodes.bikewars.errors.ResourceNotFoundException;
 import org.yogocodes.bikewars.model.UserInfoModel;
 import org.yogocodes.bikewars.services.UserInfoService;
+import org.yogocodes.bikewars.util.UserSessionUtil;
+import org.yogocodes.bikewars.web.AbstractBaseController;
 
 @Controller
-public class UserInfoController {
+public class UserInfoController extends AbstractBaseController {
 
 	private final Logger log = LoggerFactory.getLogger(UserInfoController.class);
 
@@ -21,9 +26,23 @@ public class UserInfoController {
 	@RequestMapping(value = "/personal/userinfos/{userId}")
 	@ResponseBody
 	public UserInfoModel getUserInfo(@PathVariable final Long userId) {
-
+		log.trace("getting user info for user: {}", userId);
 		final UserInfoModel userInfo = getUserInfoService().getUserInfo(userId);
 		return userInfo;
+	}
+
+	@RequestMapping(value = "/personal/userinfos/current")
+	@ResponseBody
+	public UserInfoModel getUserInfo(final HttpSession session) throws ResourceNotFoundException {
+
+		final Long userId = UserSessionUtil.getUserId(session);
+
+		if (userId == null) {
+			throw new ResourceNotFoundException("Can't find user id for anonymous user");
+		}
+
+		log.trace("getting user info for current session: {}", userId);
+		return getUserInfo(userId);
 	}
 
 	public UserInfoService getUserInfoService() {
