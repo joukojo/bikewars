ac66df9088172062ec1a0f3cfca6507def46e84e
Jouko Johansson <joukojo@gmail.com>
2013-08-15 08:30:19 +0300

    added serial version

--- src/main/java/org/yogocodes/bikewars/errors/ResourceNotFoundException.java
+++ src/main/java/org/yogocodes/bikewars/errors/ResourceNotFoundException.java
@@ -2,6 +2,11 @@ package org.yogocodes.bikewars.errors;
 
 public class ResourceNotFoundException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6113260985576938680L;
+
 	public ResourceNotFoundException(final String msg) {
 		super(msg);
 	}
