66f842ed470e49f8d826e576121b1bde121d3414
Jouko Johansson <joukojo@gmail.com>
2013-08-16 14:01:15 +0300

    made project refactor

--- src/main/java/org/yogocodes/bikewars/scheduler/EnergySchedulerTask.java
+++
@@ -1,7 +0,0 @@
-package org.yogocodes.bikewars.scheduler;
-
-public class EnergySchedulerTask {
-	// Query
-	// update user_profile set energy = if( energy + energy_inc < max_energy,
-	// energy+energy_inc, max_energy ) where energy < max_energy ;
-}
