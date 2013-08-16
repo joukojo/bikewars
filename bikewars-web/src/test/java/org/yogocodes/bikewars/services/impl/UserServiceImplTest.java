670af034b80e695d46ca365d929ca3c9340485c3
Jouko Johansson <joukojo@gmail.com>
2013-08-16 14:23:41 +0300

    Fixed the junit test

--- bikewars-web/src/test/java/org/yogocodes/bikewars/services/impl/UserServiceImplTest.java
+++ bikewars-web/src/test/java/org/yogocodes/bikewars/services/impl/UserServiceImplTest.java
@@ -5,9 +5,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.yogocodes.bikewars.dao.UserDao;
 import org.yogocodes.bikewars.model.UserModel;
+import org.yogocodes.bikewars.util.UserModelUtil;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
@@ -20,9 +22,9 @@ public class UserServiceImplTest {
 	public void testGetUser() {
 		final Long sessionUserId = System.currentTimeMillis();
 
+		final UserModel userModel = UserModelUtil.createUser(sessionUserId);
+		Mockito.when(userDao.getUser(sessionUserId)).thenReturn(userModel);
 		final UserModel actualUser = userServiceImpl.getUser(sessionUserId);
-
-		final UserModel userModel = userDao.getUser(sessionUserId);
 
 		Assert.assertNotNull("acutal user is null", actualUser);
 		Assert.assertEquals("actual user is altered", userModel, actualUser);
