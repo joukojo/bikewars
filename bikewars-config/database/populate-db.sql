934d0fa4e394ec56b4a003daa2fe5e8d232fe19a
Jouko Johansson <joukojo@gmail.com>
2013-08-22 17:21:57 +0300

    initial populate sqls

---
+++ bikewars-config/database/populate-db.sql
@@ -0,0 +1,22 @@
+insert into users(email, password, created, modified) values ('user1@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user2@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user3@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user4@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user5@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user6@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user7@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user8@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user9@bikewars.net', 'password', NOW(), NOW()); 
+insert into users(email, password, created, modified) values ('user10@bikewars.net', 'password', NOW(), NOW()); 
+
+
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (1, 'user1', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (2, 'user2', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (3, 'user3', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (4, 'user4', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (5, 'user5', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (6, 'user6', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (7, 'user7', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (8, 'user8', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (9, 'user9', 1, 100, 10, 1, 100, 10); 
+insert into user_profile(id, nickname, energy, max_energy, energy_inc, health, max_health, health_inc) values (10, 'user10', 1, 100, 10, 1, 100, 10); 
