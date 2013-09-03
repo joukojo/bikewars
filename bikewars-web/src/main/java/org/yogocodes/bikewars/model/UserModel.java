3a4c93b8fb2b2028f3cdf1b92495fd33203c1f0e
Jouko Johansson <joukojo@gmail.com>
2013-09-03 17:10:30 +0300

    Added the jobmodel to be serializable

--- bikewars-web/src/main/java/org/yogocodes/bikewars/model/UserModel.java
+++ bikewars-web/src/main/java/org/yogocodes/bikewars/model/UserModel.java
@@ -1,47 +1,62 @@
 package org.yogocodes.bikewars.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-public class UserModel {
+public class UserModel implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private Long userId;
 	private String email;
-	private String password; 
-	private Date created; 
+	private String password;
+	private Date created;
 	private Date modified;
-	
+
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
+
+	public void setEmail(final String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
+
+	public void setPassword(final String password) {
 		this.password = password;
 	}
+
 	public Date getCreated() {
 		return created;
 	}
-	public void setCreated(Date created) {
+
+	public void setCreated(final Date created) {
 		this.created = created;
 	}
+
 	public Date getModified() {
 		return modified;
 	}
-	public void setModified(Date modified) {
+
+	public void setModified(final Date modified) {
 		this.modified = modified;
 	}
+
 	public Long getUserId() {
 		return userId;
 	}
-	public void setUserId(Long userId) {
+
+	public void setUserId(final Long userId) {
 		this.userId = userId;
-	} 
-	
+	}
+
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
