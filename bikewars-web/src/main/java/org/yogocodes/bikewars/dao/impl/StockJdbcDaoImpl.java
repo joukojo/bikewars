aa131481a7c489b16b9ec3ae8faccb4f1af54048
Jouko Johansson <joukojo@gmail.com>
2013-09-20 23:49:08 +0300

    Removed unecessary imports

--- bikewars-web/src/main/java/org/yogocodes/bikewars/dao/impl/StockJdbcDaoImpl.java
+++ bikewars-web/src/main/java/org/yogocodes/bikewars/dao/impl/StockJdbcDaoImpl.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Repository;
 import org.yogocodes.bikewars.dao.StockDao;
 import org.yogocodes.bikewars.model.StockModel;
