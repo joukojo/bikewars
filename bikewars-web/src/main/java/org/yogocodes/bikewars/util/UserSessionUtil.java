9346128fdc69cfac532f0a9eb8db660938c2c575
Jouko Johansson <joukojo@gmail.com>
2013-09-22 23:55:45 +0300

    removed final keyword

--- bikewars-web/src/main/java/org/yogocodes/bikewars/util/UserSessionUtil.java
+++ bikewars-web/src/main/java/org/yogocodes/bikewars/util/UserSessionUtil.java
@@ -12,7 +12,7 @@ public class UserSessionUtil {
 	private UserSessionUtil() {
 	}
 
-	public static final Long getUserId(final HttpSession httpSession) {
+	public static Long getUserId(final HttpSession httpSession) {
 		final Object attributeValue = httpSession.getAttribute(USER_ID_SESSION_ATTR);
 		if (attributeValue == null) {
 			return null;
