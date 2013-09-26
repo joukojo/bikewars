6202ee0467b8b5a04b58efb9bf274c4a5f518076
Jouko Johansson <joukojo@gmail.com>
2013-09-26 22:40:14 +0300

    added the fight result model

---
+++ bikewars-web/src/main/java/org/yogocodes/bikewars/dao/impl/FightResultRowMapper.java
@@ -0,0 +1,38 @@
+package org.yogocodes.bikewars.dao.impl;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.yogocodes.bikewars.model.FightResultModel;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joukojo
+ * Date: 9/23/13
+ * Time: 12:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FightResultRowMapper implements RowMapper<FightResultModel> {
+    @Override
+    public FightResultModel mapRow(ResultSet resultSet, int i) throws SQLException {
+        FightResultModel fightResultModel = new FightResultModel();
+
+        Integer id = resultSet.getInt("id");
+        Long attackerId = resultSet.getLong("attacker_id");
+        Long defenderId = resultSet.getLong("defender_id");
+        Boolean attackerWon = resultSet.getBoolean("attacker_won");
+        Long money = resultSet.getLong("money");
+        // Integer health = resultSet.getInt("health");
+        Date created = resultSet.getDate("created");
+
+        fightResultModel.setAttackerId(attackerId);
+        fightResultModel.setAttackerWon(attackerWon);
+        fightResultModel.setCreated(created);
+        fightResultModel.setDefenderId(defenderId);
+        fightResultModel.setMoney(money);
+
+        return fightResultModel;
+    }
+}
