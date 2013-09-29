015b26176aa57508589db66f39cf54e98cd11be7
Jouko Johansson <joukojo@gmail.com>
2013-09-29 22:09:02 +0300

    added citycontroller

--- bikewars-web/src/main/java/org/yogocodes/bikewars/web/personal/CityController.java
+++ bikewars-web/src/main/java/org/yogocodes/bikewars/web/personal/CityController.java
@@ -1,5 +1,20 @@
 package org.yogocodes.bikewars.web.personal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CityController {
 
+	private final Logger log = LoggerFactory.getLogger(CityController.class);
+	
+	
+	@RequestMapping(value="/personal/cities", method=RequestMethod.GET)
+	public String view() {
+		log.trace("view cities");
+		return "personal/cities";
+	}
 }
